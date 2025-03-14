d3ce82ebbc67d27eefc6f3e1e3e2f89f4043af35
FlowrPro <fiestaadam10@gmail.com>
2025-03-14 14:24:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ let mobs = [];
 let petals = [];
 
 function generateMobs() {
-    if(mobs.length < 10){
-        mobs.push({x: Math.random() * 800, y: Math.random() * 600})
+    if (mobs.length < 10) {
+        mobs.push({ x: Math.random() * 800, y: Math.random() * 600 });
     }
 }
 
 function generatePetals() {
-    if(petals.length < 20){
-        petals.push({x: Math.random() * 800, y: Math.random() * 600})
+    if (petals.length < 20) {
+        petals.push({ x: Math.random() * 800, y: Math.random() * 600 });
     }
 }
 
@@ -43,7 +43,9 @@ io.on('connection', (socket) => {
             socket.emit('registerFailure', 'Username already exists');
         } else {
             users[data.username] = { username: data.username, password: data.password, x: 100, y: 100, petals: 0, inventory: [] };
-            socket.emit('registerSuccess', users[data.username]);
+            //Immediately send the login success
+            socket.username = data.username;
+            socket.emit('loginSuccess', users[data.username]);
         }
     });
 
@@ -52,7 +54,6 @@ io.on('connection', (socket) => {
             users[socket.username].x = data.x;
             users[socket.username].y = data.y;
 
-            // Simple petal collection
             petals = petals.filter(petal => {
                 if (Math.abs(users[socket.username].x - petal.x) < 20 && Math.abs(users[socket.username].y - petal.y) < 20) {
                     users[socket.username].petals++;
@@ -60,18 +61,21 @@ io.on('connection', (socket) => {
                 }
                 return true;
             });
-            //simple mob collision
+
             mobs = mobs.filter(mob => {
-                if (Math.abs(users[socket.username].x - mob.x) < 20 && Math.abs(users[socket.username].y - mob.y) < 20){
+                if (Math.abs(users[socket.username].x - mob.x) < 20 && Math.abs(users[socket.username].y - mob.y) < 20) {
                     return false;
                 }
                 return true;
             });
-
         }
     });
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
 });
